dc43ebac71afa3a6a5b993b618d04a405b03870a
TranTung31 <trantung310502@gmail.com>
2024-11-07 09:04:08 +0700

    feat: viết api chỉnh sửa profile

---
+++ src/StayCloudAPI.Application/DTOs/Content/ProfileDto/ProfileRequestDto.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StayCloudAPI.Application.DTOs.Content.ProfileDto
+{
+    public class ProfileRequestDto
+    {
+        public string UserName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string PhoneNumber { get; set; } = string.Empty;
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public IFormFile? Avatar { get; set; }
+    }
+}
---
+++ src/StayCloudAPI.Application/DTOs/Content/ProfileDto/ProfileResponseDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StayCloudAPI.Application.DTOs.Content.ProfileDto
+{
+    public class ProfileResponseDto
+    {
+        public Guid Id { get; set; }
+        public string UserName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string PhoneNumber { get; set; } = string.Empty;
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Avatar { get; set; } = string.Empty;
+        public DateTime DateCreated { get; set; }
+    }
+}
